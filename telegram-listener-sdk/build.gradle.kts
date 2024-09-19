ad786cdff29e9555a20b4b2aeebce5a1d6107f63
Artem Klimenko <17786225+Velkonost@users.noreply.github.com>
2024-09-19 21:18:24 +0500

    update gradle

--- telegram-listener-sdk/build.gradle.kts
+++ telegram-listener-sdk/build.gradle.kts
@@ -1,4 +1,66 @@
 plugins {
     id("default-convention")
     id("serialization-convention")
+    id("maven-publish")
+    alias(libs.plugins.jreleaser)
+}
+
+java {
+    withJavadocJar()
+    withSourcesJar()
+}
+
+group = "io.github.velkonost"
+version = libs.versions.telegram.listener.sdk.get()
+description = "Simple TDLib implementation in Kotlin for listen messages"
+
+publishing {
+
+    publications {
+        create<MavenPublication>("release") {
+            from(components["java"])
+
+            groupId = "io.github.velkonost"
+            artifactId = "telegram-listener-sdk"
+
+            pom {
+                name.set(project.properties["POM_NAME"].toString())
+                description.set(project.description)
+                url.set("https://github.com/Velkonost/telegram-listener-sdk")
+                issueManagement {
+                    url.set("https://github.com/Velkonost/telegram-listener-sdk/issues")
+                }
+
+                scm {
+                    url.set("https://github.com/Velkonost/telegram-listener-sdk")
+                    connection.set("scm:git://github.com/Velkonost/telegram-listener-sdk.git")
+                    developerConnection.set("scm:git://github.com/Velkonost/telegram-listener-sdk.git")
+                }
+
+                licenses {
+                    license {
+                        name.set("The Apache Software License, Version 2.0")
+                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                        distribution.set("repo")
+                    }
+                }
+
+                developers {
+                    developer {
+                        id.set("velkonost")
+                        name.set("Artem Klimenko")
+                        email.set("velkonost@gmail.com")
+                        url.set("t.me/velkonost")
+                    }
+                }
+            }
+
+        }
+    }
+
+    repositories {
+        maven {
+            setUrl(layout.buildDirectory.dir("staging-deploy"))
+        }
+    }
 }
