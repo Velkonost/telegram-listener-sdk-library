4d0fa2edaedc2f9e354421bb96a554301b9ac481
Artem Klimenko <17786225+Velkonost@users.noreply.github.com>
2024-09-20 19:15:53 +0500

    add linux os

--- telegram-listener-sdk/src/main/kotlin/velkonost/telegram/sdk/listener/TGListenerSDK.kt
+++ telegram-listener-sdk/src/main/kotlin/velkonost/telegram/sdk/listener/TGListenerSDK.kt
@@ -22,14 +22,14 @@ object TGListenerSDK {
         val libFolder = when {
             osName.contains("linux") && arch.contains("x86_64") -> "linux_x64"
             osName.contains("linux") && arch.contains("arm64") -> "linux_arm64"
+            osName.contains("linux") -> "linux_x64"
             osName.contains("mac") && arch.contains("aarch64") -> "macos_silicon"
             osName.contains("windows") && arch.contains("amd64") -> "windows_x64"
             else -> throw UnsupportedOperationException("Unsupported OS or architecture: $osName-$arch")
         }
 
         val libExtension = when {
-            osName.contains("linux") && arch.contains("x86_64") -> "iso"
-            osName.contains("linux") && arch.contains("arm64") -> "iso"
+            osName.contains("linux") -> "iso"
             osName.contains("mac") && arch.contains("aarch64") -> "dylib"
             osName.contains("windows") && arch.contains("amd64") -> "dll"
             else -> throw UnsupportedOperationException("Unsupported OS or architecture: $osName-$arch")
